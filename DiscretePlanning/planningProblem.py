14a5356cf00697bfbc1b61adaf1b0068801ae37f
HateFregeLoveRussell <hatefregeloverussell@outlook.com>
2024-09-04 00:18:51 -0400

    Refactored Problem class to accept non-finite state spaces that need not necessarily be explicitly represented in memory

--- DiscretePlanning/planningProblem.py
+++ DiscretePlanning/planningProblem.py
@@ -7,39 +7,68 @@ class DiscretePlanningProblem:
 
         Attributes
         ----------
-        stateSpace : dict
-            Dictionary representing the state space where keys are states and values are lists of possible actions.
-        transitionFunction : str -> list
+        belongingFunction : X -> bool
+            A function that takes a state and returns a boolean corresponding to if it belongs to the state space X or not
+        actionFunction : X -> U(x)
+            A function that takes a state and return a set object corresponding to the actions that can be preformed from this state i.e U(x)
+        transitionFunction : X x U(x) -> X
             A function that takes a state and an action as input and returns the resulting state.
-        initialState : str
+        initialState : 
             The initial state of the problem represented as a string as in stateSpace
-        goalStates : list
+        goalStates : set
             A list of goal states for the planning Problem
+        actionSpace : set
+            A set object corresponding to the union of action sets across all sets
             
         Methods
         -------
         TODO write methods descriptions
     """
 
-    def __init__(self,stateSpace: dict, transitionFunction: Callable, initialState: str, goalStates: list):
+    def __init__(self,belongingFunction: Callable, actionFunction: Callable, transitionFunction: Callable, initialState, goalStates: set, actionSpace: set = None):
         """
         Initialize the planning problem.
         
-        :param stateSpace: Dictionary representing the state space where keys are states and values are lists of possible actions.
+        :param belongingFunction: A function that takes a state and returns a boolean corresponding to if it belongs to the state space X or not
+        :param actionFunction: A function that takes a state and return a set object corresponding to the actions that can be preformed from this state i.e U(x)
         :param transitionFunction: A function that takes a state and an action as input and returns the resulting state.
         :param initialState: The initial state of the problem represented as a string as in stateSpace
-        :param goalStates: A list of goal states for the planning Problem
+        :param goalStates: A set of goal states for the planning Problem
+        :param actionSpace: A set object corresponding to the union of action sets across all sets - optional
         """
-        # dictionary where state x maps to action space U(x)
-        self.stateSpace = stateSpace
+        # belonging function f: X -> T/F
+        self.belongingFunction = belongingFunction
+
+        # action function f: X -> U(x)
+        self.actionFunction = actionFunction
+
         # transition function f: X x U(x) -> X
         self.transitionFunction = transitionFunction
 
-        if initialState not in self.stateSpace:
+        if not belongingFunction(initialState):
             raise ValueError("Initial State not in State Space")
         self.initialState = initialState
         
-        if not all(state in stateSpace for state in goalStates):
+        if not all(belongingFunction(state) for state in goalStates):
             raise ValueError("Some Goal state is not in the State Space")
         self.goalStates = goalStates
+        
+        self.actionSpace = actionSpace
+
+
+
+    def get_next_states(self, state) -> list: 
+        """
+        Given a state, return an array of possible next states.
+        
+        :param state: The current state.
+        :return: List of possible next states.
+        """
+        if not self.belongingFunction(state):
+            raise ValueError("State Not Found in State Space")
+        
+        actions = self.actionFunction(state)
+
+        return [self.transitionFunction(state,action) for action in actions]
+
 
--- Tests/test_DiscretePLanningProblem.py
+++ Tests/test_DiscretePLanningProblem.py
@@ -1,5 +1,6 @@
 import unittest
 from DiscretePlanning.planningProblem import DiscretePlanningProblem
+
 def test_transition_function(state, action):
     match (state,action):
         case ('A','right'):
@@ -8,42 +9,67 @@ def test_transition_function(state, action):
             return 'C'
         case ('C', 'left'):
             return 'B'
+        case ('C', 'right'):
+            return 'D'
         case ('D', 'left'):
             return 'C'
         case _:
             raise ValueError("state action transition not defined")
+
+def test_action_function(state) -> set:
+    match(state):
+        case('A'):
+            return set(['right'])
+        case('B'):
+            return set(['right'])
+        case('C'):
+            return set(['right', 'left'])
+        case('D'):
+            return set(['left'])
+        case('Z'):
+            return set([])
+        case _:
+            raise ValueError("state actions not defined")
+        
         
 class TestDiscretePlanningProblem(unittest.TestCase):
     def setUp(self):
-        # A -> B <-> C <- D
-        self.stateSpace = {'A' : ['right'], 'B': [ 'right'], 'C': ['left'], 'D': ['left']}
+        # A -> B <-> C <-> D . . . Z
+        self.belongingFunction = lambda state: state in ['A', 'B', 'C', 'D', 'Z']
         self.transitionFunction = test_transition_function
+        self.actionFunction= test_action_function
+        self.actionSpace = set(['left','right'])
         self.initialState = 'A'
-        self.goalStates = ['D', 'B']
-    
+        self.goalStates = set(['D', 'B'])
+
+    #INIT tests
     def test_init_success(self):
         """Test successful initialization of DiscretePlanningProblem."""
         planningProblem = DiscretePlanningProblem(
-            self.stateSpace,
-            self.transitionFunction,
-            self.initialState,
-            self.goalStates
+            actionFunction=self.actionFunction,
+            belongingFunction=self.belongingFunction,
+            transitionFunction=self.transitionFunction,
+            actionSpace=self.actionSpace,
+            initialState=self.initialState,
+            goalStates=self.goalStates
         )
         self.assertEqual(planningProblem.initialState, self.initialState)
-        self.assertEqual(planningProblem.goalStates, self.goalStates)
-        self.assertEqual(planningProblem.stateSpace, self.stateSpace)
+        self.assertSetEqual(planningProblem.goalStates, self.goalStates)
+        self.assertEqual(planningProblem.actionFunction, self.actionFunction)
+        self.assertEqual(planningProblem.belongingFunction, self.belongingFunction)
         self.assertEqual(planningProblem.transitionFunction, self.transitionFunction)
-
-        
+        self.assertSetEqual(planningProblem.actionSpace, set(['left','right']))
 
     def test_init_fail_initial_state(self):
         """Test initialization failure when the initial state is invalid."""
         with self.assertRaises(ValueError) as context:
             planningProblem = DiscretePlanningProblem(
-                stateSpace = self.stateSpace,
-                transitionFunction = self.transitionFunction,
-                initialState = 'Z',
-                goalStates = self.goalStates
+                actionFunction=self.actionFunction,
+                belongingFunction=self.belongingFunction,
+                transitionFunction=self.transitionFunction,
+                actionSpace=self.actionSpace,
+                initialState='1',
+                goalStates=self.goalStates
             )
             self.assertTrue('Initial State not in State Space' in str(context.exception))
 
@@ -51,12 +77,67 @@ class TestDiscretePlanningProblem(unittest.TestCase):
         """Test initialization failure when one or more goal states are invalid."""
         with self.assertRaises(ValueError) as context:
             planningProblem = DiscretePlanningProblem(
-                stateSpace = self.stateSpace,
-                transitionFunction = self.transitionFunction,
-                initialState = self.initialState,
-                goalStates = ['D', 'A', 'Z']
+                actionFunction=self.actionFunction,
+                belongingFunction=self.belongingFunction,
+                transitionFunction=self.transitionFunction,
+                actionSpace=self.actionSpace,
+                initialState=self.initialState,
+                goalStates = set(['D', 'A', '1', '2'])
             )
             self.assertTrue('One or more Goal States not in State Space' in str(context.exception))
+
+    #get_next_states tests
+    def test_get_next_states_success_single(self):
+        """Test get_next_states() with single transition per state"""
+        planningProblem = DiscretePlanningProblem(
+            actionFunction=self.actionFunction,
+            belongingFunction=self.belongingFunction,
+            transitionFunction=self.transitionFunction,
+            actionSpace=self.actionSpace,
+            initialState=self.initialState,
+            goalStates=self.goalStates
+        )
+        self.assertEqual(planningProblem.get_next_states('A'), ['B'])
+        self.assertEqual(planningProblem.get_next_states('B'), ['C'])
+        self.assertEqual(planningProblem.get_next_states('D'), ['C'])
+    
+    def test_get_next_states_success_multi(self):
+        """Test get_next_states() with multiple transition state"""
+        planningProblem = DiscretePlanningProblem(
+            actionFunction=self.actionFunction,
+            belongingFunction=self.belongingFunction,
+            transitionFunction=self.transitionFunction,
+            actionSpace=self.actionSpace,
+            initialState=self.initialState,
+            goalStates=self.goalStates
+        )
+        self.assertCountEqual(planningProblem.get_next_states('C'),['B','D'])
+        self.assertEqual(sorted(planningProblem.get_next_states('C')), sorted(['B','D']))
+
+    def test_get_next_states_isolated_state(self):
+        """Test get_next_states() with Isolated State"""
+        planningProblem = DiscretePlanningProblem(
+            actionFunction=self.actionFunction,
+            belongingFunction=self.belongingFunction,
+            transitionFunction=self.transitionFunction,
+            actionSpace=self.actionSpace,
+            initialState=self.initialState,
+            goalStates=self.goalStates
+        )
+        self.assertEqual(planningProblem.get_next_states('Z'), [])
+    
+    def test_get_next_states_failure(self):
+        planningProblem = DiscretePlanningProblem(
+            actionFunction=self.actionFunction,
+            belongingFunction=self.belongingFunction,
+            transitionFunction=self.transitionFunction,
+            actionSpace=self.actionSpace,
+            initialState=self.initialState,
+            goalStates=self.goalStates
+        )
+        with self.assertRaises(ValueError) as context:
+            planningProblem.get_next_states('1')
+            self.assertTrue("State Not Found in State Space" in str(context.exception))
 
 if __name__ == '__main__':
     unittest.main()
