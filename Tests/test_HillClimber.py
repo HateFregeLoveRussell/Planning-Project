b299be7151edaeada3b3d0cc1e8633b0130fae5c
HateFregeLoveRussell <HateFregeLoveRussell@outlook.com>
2024-10-14 23:07:28 -0400

    Fixed Small representation bug in HillClimber Test

--- Tests/test_HillClimber.py
+++ Tests/test_HillClimber.py
@@ -5,9 +5,12 @@ from pathlib import Path
 import numpy as np
 from ast import literal_eval
 
+from sympy.physics.units import liter
+
+
 # Define the height function
 def test_height_function(x: int, y: int) -> float:
-    peak1 = 20 * np.exp(-((x - 5)**2 + (y - 5)**2) / 20)
+    peak1 = 20 * np.exp(-((x - 8)**2 + (y - 8)**2) / 20)
     peak2 = 8 * np.exp(-((x - 15)**2 + (y - 15)**2) / 30)
     peak3 = 2.3 * np.exp(-((x - 10)**2 + (y - 10)**2) / 15)
     return peak1 + peak2 + peak3
@@ -16,19 +19,19 @@ class test_HillClimber(unittest.TestCase):
         def setUp(self):
             pass
         def test(self):
-            initial_state = repr((0, 0))
-            goalStates = {repr((29, 29))}
-            size = (30, 30)
+            initial_state = repr((1, 19))
+            goalStates = {repr((19, 1))}
+            size = (20, 20)
             height_function = test_height_function
 
             def HueristicFunction(state: str) -> float:
                 x, y = literal_eval(state)[0], literal_eval(state)[1]
                 coordinates = np.array([x, y, height_function(x, y)])
-                x_prime, y_prime = 29, 29
+                x_prime, y_prime = literal_eval(next(iter(goalStates)))[0], literal_eval(next(iter(goalStates)))[1]
                 coordinates_prime = np.array([x_prime, y_prime, height_function(x_prime, y_prime)])
                 return float(np.linalg.norm(coordinates_prime - coordinates))
 
-            logFile = Path("DiscretePlanning/Environment/HillClimberEnvironment/Logs/HillClimberFirstTest.json")
+            logFile = Path("DiscretePlanning/Environment/HillClimberEnvironment/Logs/HillClimberSecondTest.json")
             create_parent = True
 
             climber = HillClimber(height_function, size, initial_state, goalStates)
